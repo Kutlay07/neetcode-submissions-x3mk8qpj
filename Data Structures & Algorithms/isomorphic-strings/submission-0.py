66f328eb42df2d18db0145576a9b91afd65d4c0c
Kutlay07 <kutlay-mahmut07@hotmail.com>
2026-04-11 17:28:29 +0300

    Add: isomorphic-strings - submission-0

---
+++ Data Structures & Algorithms/isomorphic-strings/submission-0.py
@@ -0,0 +1,14 @@
+class Solution:
+    def isIsomorphic(self, s: str, t: str) -> bool:
+        s_to_t = {}
+        t_to_s = {}
+
+        for sc, tc in zip(s,t):
+            
+            if sc in s_to_t and s_to_t[sc] != tc:
+                return False
+            if tc in t_to_s and t_to_s[tc] != sc:
+                return False
+            s_to_t[sc] = tc
+            t_to_s[tc] = sc
+        return True
