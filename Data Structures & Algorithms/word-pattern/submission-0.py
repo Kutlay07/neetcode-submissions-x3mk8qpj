8374c9f883a82312b9a4d5c4ba488ce4cdf9723b
Kutlay07 <kutlay-mahmut07@hotmail.com>
2026-05-02 11:53:11 +0300

    Add: word-pattern - submission-0

---
+++ Data Structures & Algorithms/word-pattern/submission-0.py
@@ -0,0 +1,23 @@
+class Solution:
+    def wordPattern(self, pattern: str, s: str) -> bool:
+        p2w = {}
+        w2p = {}
+        words = s.split()
+
+        if len(s.split()) != len(pattern):
+            return False
+
+        for i in range(len(pattern)):
+            p = pattern[i]
+            w = words[i]
+
+            if p not in p2w:
+                p2w[p] = w
+            
+            if w not in w2p:
+                w2p[w] = p
+
+            if p2w[p] != w or w2p[w] != p:
+                return False
+
+        return True
